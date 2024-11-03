52fa3b6756a7abb1acba652543a3191f4e5c4cd4
Tobias <tobiasfunes@hotmail.com.ar>
2024-11-03 10:06:20 -0300

    archivos de preubas

---
+++ input/errores.c
@@ -0,0 +1,11 @@
+// Error: falta paréntesis
+while a > 0 {  // Error: falta paréntesis
+    a = a - 1;
+}
+
+// Error: formato incorrecto en lista de variables
+int a, , b;  // Error: comas consecutivas
+int 123var;  // Error: nombre de variable inválido
+int x y;     // Error: falta coma entre variables
+
+// Error: llamada a función
---
+++ input/prueba.c
@@ -0,0 +1,20 @@
+// Comentario que deberia ser ignorado
+
+char hola = 10;
+
+void main () {
+
+    char t = 65;
+    int a = 10;
+    int b = 10;
+
+    while (b)
+    {
+        a = a + b;
+        return 0;
+    }
+    
+}
+
+
+
